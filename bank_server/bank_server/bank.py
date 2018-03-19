83965fbe0ef9dce30137517520baacbe8303f70f
danielritter14 <danielritter14@gmail.com>
2018-03-19 19:27:40 -0400

    fix bank

--- bank_server/bank_server/bank.py
+++ bank_server/bank_server/bank.py
@@ -88,6 +88,14 @@ class Bank(object):
                 return response
 
             # authenticate the card
+            print account['chall']
+            print request.json['encrypted_response']
+            print account['AES_KEY']
+            key = str(bytearray.fromhex(account['AES_KEY']))
+            cipher = AES.new(key, AES.MODE_ECB, '')
+            decrypted_response = cipher.decrypt(str(bytearray.fromhex(request.json['encrypted_response'])))
+            print decrypted_response
+
             if not db.verify_challenge(account['chall'], request.json['encrypted_response'], account['AES_KEY'], account['time']):
                 time.sleep(2)
                 return response
